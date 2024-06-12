164fd2b3daf3f224448bcf567c8dd7161dd1b72e
Z-Bahaa <zizobahaapersonal@gmail.com>
2024-06-12 22:21:07 +0200

    removed request timeout duration

--- index.js
+++ index.js
@@ -37,7 +37,6 @@ app.get('/scrape-product', async (req, res) => {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
       },
-      timeout: 5000,
     });
 
     const html = response.data;
